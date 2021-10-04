08575cae96406a9f55d7d3377f5daef3e0bc3a76
Anastasiia <nastya.kolodezhnaya@gmail.com>
2021-10-04 23:02:56 +0300

    lesson 10, students

--- students/models.py
+++ students/models.py
@@ -7,3 +7,12 @@ class Student(models.Model):
     last_name = models.CharField(max_length=80, null=False)
     email = models.EmailField(max_length=120, null=True)
     birthdate = models.DateField(null=True, default=datetime.date.today())
+
+    def __str__(self):
+        return f'{self.full_name()}, {self.id}'
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def age(self):
+        return datetime.datetime.year - self.birthdate.year
