51d48f1d28161a3dc38dbd1589aa14ead021d3cc
Anastasiia <nastya.kolodezhnaya@gmail.com>
2021-10-04 22:06:08 +0300

    lesson 9, hw 9

--- students/views.py
+++ students/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+import faker
+
+
+# Create your views here
+def start(request):
+    return HttpResponse('SUCCESS')
+
+
 def hello(request):
-    return HttpResponse("SUCCESS")
+    return HttpResponse("HELLO")
+
+
+def generate_students(request, count=10):
+    fake_student = faker.Faker('RU')
+    students = [fake_student.name() for n in range(14)]
+    result = '<br>'.join(students)
+    return HttpResponse(result)
