48c221ab629c31c82480fea8ea8bad92f2813811
Anastasiia <nastya.kolodezhnaya@gmail.com>
2021-10-11 23:37:50 +0300

    hw 11 edit

--- students/validators.py
+++ students/validators.py
@@ -9,11 +9,13 @@ def no_elon_validator(email):
 
 
 def prohibited_domains(email):
-    if email.endswith('@mail.ru') or email.endswith('@yandex.ru'):
-        raise ValidationError('Such domain is not accepted. Please, use another email')
+    prohibited = ['@mail.ru', '@yandex.ru']
+    if any([email.endswith(mail) for mail in prohibited]):
+        raise ValidationError('Such domain is not accepted. Please use another email domain')
 
 
 def older_than_18(birthdate):
-    eighteen_years_ago = datetime.date.today() - datetime.timedelta(days=6570)  # 6570 days in 18 years
+    YEARS_18_IN_DAYS = 365.2425 * 18
+    eighteen_years_ago = datetime.date.today() - datetime.timedelta(days=YEARS_18_IN_DAYS)  # 6570 days in 18 years
     if birthdate > eighteen_years_ago:
         raise ValidationError('A student cannot be younger than 18')
