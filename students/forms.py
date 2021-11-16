21eefb1c38773b5080225a5798c93607712f204c
Anastasiia <nastya.kolodezhnaya@gmail.com>
2021-11-16 22:11:50 +0200

    form edit

--- students/forms.py
+++ students/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm, EmailField
 from django.core.validators import ValidationError
 
-from students.models import UserProfile
+from students.models import UserProfile, CustomUser
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -11,5 +11,5 @@ class RegistrationStudentForm(UserCreationForm):
                        help_text="Registration without email is not possible!")
 
     class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        model = CustomUser
+        fields = ['email', 'password1', 'password2']
